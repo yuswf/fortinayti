405915894d6fdec9ca0ef1ce192d1197c30257a8
Yıusuf <yusu@duck.com>
2022-12-17 22:31:10 +0300

    initial files

---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+import {Head, Html, Main, NextScript} from 'next/document';
+
+export default function Document() {
+    return (
+        <Html lang="en">
+            <Head>
+                <title>
+                    Fortinayti Fetch
+                </title>
+            </Head>
+            <body>
+            <Main/>
+            <NextScript/>
+            </body>
+        </Html>
+    )
+}
