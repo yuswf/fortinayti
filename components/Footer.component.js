1a073dada4bf3370bd03187e9976b3b4f8fed257
YUSUF <yusu@duck.com>
2022-12-20 11:36:23 +0300

    fixed footer component

--- components/Footer.component.js
+++ components/Footer.component.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-function FooterComponent() {
-    return (
-        <footer className="p-5 flex justify-center items-center mt-[-27px] mb-5 underline">
-            <p><a target="_blank" href="https://github.com/yuswf/fortinayti">This website is open-source on GitHub.</a></p>
-        </footer>
-    );
-}
-
-export default FooterComponent;
-=======
 function FooterComponent() {
     const content = `<a target="_blank" href="https://github.com/yuswf/fortinayti">This website is open-source on GitHub.</a>`;
     
@@ -20,4 +9,3 @@ function FooterComponent() {
 }
 
 export default FooterComponent;
->>>>>>> 9cc3594afc9069ea0364980d37ea31a9936761d2
